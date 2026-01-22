2f4df27a90cd683fc1430d6c4b25b2b27bcecc17
Applied Computer Science <peter.dillinger2@srh.de>
2026-01-22 11:19:55 +0100

    stage 1 completed

--- JavaExample/src/main/java/extra/Game.java
+++ JavaExample/src/main/java/extra/Game.java
@@ -9,22 +9,32 @@ public class Game {
        Stage 0:
            The player has to guess the number
            with no hints provided.
+        Stage 1:
+            The player can exit with 0 as input,
+            and the number of attempts is counted and printed.
+        Stage 2:
+            The player receives feedback on whether their guess
+            is too high or too low.
      */
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Let's play a game!");
 
         int secretNumber = (int) (Math.random() * 9000) + 1000;
-
-        for (int i=0; i<50; i++) {
-            if (i == 17) continue;
-            System.out.println();
-        }
+        int numberOfAttempts = 0;
 
         while (true) {
             System.out.print("Please enter your guess: ");
             int guess = scanner.nextInt();
 
+            if (guess == 0) {
+                System.out.println("Thank you for playing");
+                System.out.println("The secret number was " + secretNumber);
+                break;
+            }
+
+            numberOfAttempts++;
             if (guess == secretNumber) {
                 System.out.println("Congratulations! You've guessed the number.");
                 break;
@@ -32,6 +42,7 @@ public class Game {
                 System.out.println("Incorrect guess. Try again.");
             }
         }
-
+        // DRY - Don't repeat yourself, do not WET (Write everything twice)
+        System.out.println("You needed " + numberOfAttempts + " number of attempts.");
     }
 }
